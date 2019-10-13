99b75840620cbc9a21714f7d4a54669f6466dcc1
texasaggie97 <github@markesilva.com>
2019-10-13 15:36:44 -0500

    Remove semicolons

--- AmbientWeatherApp.groovy
+++ AmbientWeatherApp.groovy
@@ -1,39 +1,39 @@
-definition(name: "Ambient Weather API", namespace: "CordMaster", author: "Alden Howard", description: "A simple api for providing ambient weather access", iconUrl: "", iconX2Url: "");
+definition(name: "Ambient Weather API", namespace: "CordMaster", author: "Alden Howard", description: "A simple api for providing ambient weather access", iconUrl: "", iconX2Url: "")
 
 preferences {
     page(name: "page1", title: "Log In", nextPage: "page2", uninstall: true) {
         section {
-            input(name: "applicationKey", title: "Application Key", type: "text", required: true);
-            input(name: "apiKey", title: "API Key", type: "text", required: true);
+            input(name: "applicationKey", title: "Application Key", type: "text", required: true)
+            input(name: "apiKey", title: "API Key", type: "text", required: true)
         }
     }
 
-    page(name: "page2");
-    page(name: "page3");
+    page(name: "page2")
+    page(name: "page3")
 }
 
 def page2() {
-    def stations = [];
-    def stationMacs = [];
+    def stations = []
+    def stationMacs = []
     try {
-        stations = getStations();
+        stations = getStations()
 
-        stations.each { stationMacs << it.macAddress };
+        stations.each { stationMacs << it.macAddress }
     } catch (groovyx.net.http.HttpResponseException e) {
         //then unauthorized
         return dynamicPage(name: "page2", title: "Error", nextPage: "page1", uninstall: true) {
             section {
-                paragraph("There was an error authorizing you. Please try again.");
+                paragraph("There was an error authorizing you. Please try again.")
             }
         }
     }
 
-    log.debug("Got stations: " + stations);
+    log.debug("Got stations: " + stations)
 
     return dynamicPage(name: "page2", title: "Select Station", nextPage: "page3", uninstall: true) {
         section {
-            input(name: "station", title: "Station", type: "enum", options: stationMacs, required: true);
-            input(name: "refreshInterval", title: "Refresh Interval (in minutes)", type: "number", range: "1..3600", defaultValue: 1, required: true);
+            input(name: "station", title: "Station", type: "enum", options: stationMacs, required: true)
+            input(name: "refreshInterval", title: "Refresh Interval (in minutes)", type: "number", range: "1..3600", defaultValue: 1, required: true)
         }
     }
 }
@@ -41,25 +41,25 @@ def page2() {
 def page3() {
     dynamicPage(name: "page3", title: "Confirm Settings", install: true, uninstall: true) {
         section {
-            paragraph("Selected station: $station");
-            paragraph("Refresh interval: $refreshInterval minute(s)");
+            paragraph("Selected station: $station")
+            paragraph("Refresh interval: $refreshInterval minute(s)")
         }
 
         section {
-            paragraph("Press done to finish");
+            paragraph("Press done to finish")
         }
     }
 }
 
 //lifecycle functions
 def installed() {
-    log.debug("Installed");
+    log.debug("Installed")
 
-    addDevice();
+    addDevice()
 
-    initialize();
+    initialize()
 
-    runEvery5Minutes(fetchNewWeather);
+    runEvery5Minutes(fetchNewWeather)
 }
 
 def uninstalled() {
@@ -67,33 +67,33 @@ def uninstalled() {
 }
 
 def updated() {
-    log.debug("Updated");
+    log.debug("Updated")
 
-    unsubscribe();
-    unschedule();
-    installed();
-    initialize();
+    unsubscribe()
+    unschedule()
+    installed()
+    initialize()
 }
 
 def initialize() {
-    fetchNewWeather();
+    fetchNewWeather()
 
     //chron schedule, refreshInterval is int
-    def m = refreshInterval;
-    def h = Math.floor(m / 60);
-    m -= h * 60;
+    def m = refreshInterval
+    def h = Math.floor(m / 60)
+    m -= h * 60
 
-    m = m == 0 ? "*" : "0/" + m.toInteger();
-    h = h == 0 ? "*" : "0/" + h.toInteger();
+    m = m == 0 ? "*" : "0/" + m.toInteger()
+    h = h == 0 ? "*" : "0/" + h.toInteger()
 
-    log.debug("Set CHRON schedule with m: $m and h: $h");
+    log.debug("Set CHRON schedule with m: $m and h: $h")
 
-    schedule("0 $m $h * * ? *", fetchNewWeather);
+    schedule("0 $m $h * * ? *", fetchNewWeather)
 }
 
 //children
 def addDevice() {
-    addChildDevice("CordMaster", "Ambient Weather Device", "AWTILE-$station", null, [completedSetup: true]);
+    addChildDevice("CordMaster", "Ambient Weather Device", "AWTILE-$station", null, [completedSetup: true])
 }
 
 def delDevice() {
@@ -102,29 +102,29 @@ def delDevice() {
 
 //fetch functions
 def getStations() throws groovyx.net.http.HttpResponseException {
-    def data = [];
+    def data = []
 
     def params = [
         uri: "https://api.ambientweather.net/",
         path: "/v1/devices",
         query: [applicationKey: applicationKey, apiKey: apiKey]
-    ];
+    ]
 
     requestData("/v1/devices", [applicationKey: applicationKey, apiKey: apiKey]) { response ->
-        data = response.data;
-    };
+        data = response.data
+    }
 
-    return data;
+    return data
 }
 
 def getWeather() throws groovyx.net.http.HttpResponseException {
-    def data = [];
+    def data = []
 
     requestData("/v1/devices/$station", [applicationKey: applicationKey, apiKey: apiKey, limit: 1]) { response ->
-        data = response.data;
-    };
+        data = response.data
+    }
 
-    return data[0];
+    return data[0]
 }
 
 def requestData(path, query, code) {
@@ -132,20 +132,20 @@ def requestData(path, query, code) {
         uri: "https://api.ambientweather.net/",
         path: path,
         query: query
-    ];
+    ]
 
     httpGet(params) { response ->
-        code(response);
-    };
+        code(response)
+    }
 }
 
 //loop
 def fetchNewWeather() {
 
-    def weather = getWeather();
+    def weather = getWeather()
 
-    //log.debug("Weather: " + weather);
+    //log.debug("Weather: " + weather)
 
-    childDevices[0].setWeather(weather);
+    childDevices[0].setWeather(weather)
 }
 
--- AmbientWeatherDevice.groovy
+++ AmbientWeatherDevice.groovy
@@ -44,136 +44,136 @@ metadata {
 }
 
 def refresh() {
-    parent.fetchNewWeather();
+    parent.fetchNewWeather()
 }
 
 def setWeather(weather) {
-    logger("debug", "Weather: " + weather);
+    logger("debug", "Weather: " + weather)
 
     //Set temperature
-    sendEvent(name: "temperature", value: weather.tempf, unit: '°F', isStateChange: true);
+    sendEvent(name: "temperature", value: weather.tempf, unit: '°F', isStateChange: true)
 
     //Set Humidity
-    sendEvent(name: "humidity", value: weather.humidity, unit: '%', isStateChange: true);
+    sendEvent(name: "humidity", value: weather.humidity, unit: '%', isStateChange: true)
 
     //Set DewPoint
-    sendEvent(name: "dewPoint", value: weather.dewPoint, unit:'°F', isStateChange: true);
+    sendEvent(name: "dewPoint", value: weather.dewPoint, unit:'°F', isStateChange: true)
 
     //Set Comfort Level
-    float temp = 0.0;
+    float temp = 0.0
 
-    temp = (weather.dewPoint - 35);
+    temp = (weather.dewPoint - 35)
     if (temp <= 0) {
-        temp = 0.0;
+        temp = 0.0
     } else if (temp >= 40.0) {
-        temp = 100.0;
+        temp = 100.0
     } else {
-        temp = (temp / 40.0) * 100.0;
+        temp = (temp / 40.0) * 100.0
     }
-    temp = temp.round(1);
-    sendEvent(name: "comfort", value: temp, isStateChange: true);
+    temp = temp.round(1)
+    sendEvent(name: "comfort", value: temp, isStateChange: true)
 
     //Set Barometric Pressure
-    sendEvent(name: "pressure", value: weather.baromrelin, unit: 'in', isStateChange: true);
+    sendEvent(name: "pressure", value: weather.baromrelin, unit: 'in', isStateChange: true)
 
     //Set Feels Like Temperature
-    sendEvent(name: "feelsLike", value: weather.feelsLike, unit: '°F', isStateChange: true);
+    sendEvent(name: "feelsLike", value: weather.feelsLike, unit: '°F', isStateChange: true)
 
     //Rain
-    sendEvent(name: "precip_today", value: weather.dailyrainin, unit: 'in', isStateChange: true);
-    sendEvent(name: "precip_1hr", value: weather.hourlyrainin, unit: 'in', isStateChange: true);
+    sendEvent(name: "precip_today", value: weather.dailyrainin, unit: 'in', isStateChange: true)
+    sendEvent(name: "precip_1hr", value: weather.hourlyrainin, unit: 'in', isStateChange: true)
 
     //Wind
-    sendEvent(name: "wind", value: weather.windspeedmph, unit: 'mph', isStateChange: true);
-    sendEvent(name: "wind_gust", value: weather.windgustmph, unit: 'mph', isStateChange: true);
-    sendEvent(name: "wind_degree", value: weather.winddir, unit: '°', isStateChange: true);
+    sendEvent(name: "wind", value: weather.windspeedmph, unit: 'mph', isStateChange: true)
+    sendEvent(name: "wind_gust", value: weather.windgustmph, unit: 'mph', isStateChange: true)
+    sendEvent(name: "wind_degree", value: weather.winddir, unit: '°', isStateChange: true)
 
     temp = weather.winddir
     if (temp < 22.5) {
-        sendEvent(name:  "wind_direction", value: "North", isStateChange: true);
-        sendEvent(name:  "wind_dir", value: "N", isStateChange: true);
+        sendEvent(name:  "wind_direction", value: "North", isStateChange: true)
+        sendEvent(name:  "wind_dir", value: "N", isStateChange: true)
     } else if (temp < 67.5) {
-        sendEvent(name:  "wind_direction", value: "Northeast", isStateChange: true);
-        sendEvent(name:  "wind_dir", value: "NE", isStateChange: true);
+        sendEvent(name:  "wind_direction", value: "Northeast", isStateChange: true)
+        sendEvent(name:  "wind_dir", value: "NE", isStateChange: true)
     } else if (temp < 112.5) {
-        sendEvent(name: "wind_direction", value: "East", isStateChange: true);
-        sendEvent(name:  "wind_dir", value: "E", isStateChange: true);
+        sendEvent(name: "wind_direction", value: "East", isStateChange: true)
+        sendEvent(name:  "wind_dir", value: "E", isStateChange: true)
     } else if (temp < 157.5) {
-        sendEvent(name: "wind_direction", value: "Southeast", isStateChange: true);
-        sendEvent(name:  "wind_dir", value: "SE", isStateChange: true);
+        sendEvent(name: "wind_direction", value: "Southeast", isStateChange: true)
+        sendEvent(name:  "wind_dir", value: "SE", isStateChange: true)
     } else if (temp < 202.5) {
-        sendEvent(name: "wind_direction", value: "South", isStateChange: true);
-        sendEvent(name:  "wind_dir", value: "S", isStateChange: true);
+        sendEvent(name: "wind_direction", value: "South", isStateChange: true)
+        sendEvent(name:  "wind_dir", value: "S", isStateChange: true)
     } else if (temp < 247.5) {
-        sendEvent(name: "wind_direction", value: "Southwest", isStateChange: true);
-        sendEvent(name:  "wind_dir", value: "SW", isStateChange: true);
+        sendEvent(name: "wind_direction", value: "Southwest", isStateChange: true)
+        sendEvent(name:  "wind_dir", value: "SW", isStateChange: true)
     } else if (temp < 292.5) {
-        sendEvent(name: "wind_direction", value: "West", isStateChange: true);
-        sendEvent(name:  "wind_dir", value: "W", isStateChange: true);
+        sendEvent(name: "wind_direction", value: "West", isStateChange: true)
+        sendEvent(name:  "wind_dir", value: "W", isStateChange: true)
     } else if (temp < 337.5) {
-        sendEvent(name: "wind_direction", value: "Northwest", isStateChange: true);
-        sendEvent(name:  "wind_dir", value: "NW", isStateChange: true);
+        sendEvent(name: "wind_direction", value: "Northwest", isStateChange: true)
+        sendEvent(name:  "wind_dir", value: "NW", isStateChange: true)
     } else                      {
-        sendEvent(name:  "wind_direction", value: "North", isStateChange: true);
-        sendEvent(name:  "wind_dir", value: "N", isStateChange: true);
+        sendEvent(name:  "wind_direction", value: "North", isStateChange: true)
+        sendEvent(name:  "wind_dir", value: "N", isStateChange: true)
     }
 
     //UV and Light
-    sendEvent(name: "solarradiation", value: weather.solarradiation, isStateChange: true);
-    sendEvent(name: "illuminance", value: weather.solarradiation, isStateChange: true);
-    sendEvent(name: "uv", value: weather.uv, isStateChange: true);
+    sendEvent(name: "solarradiation", value: weather.solarradiation, isStateChange: true)
+    sendEvent(name: "illuminance", value: weather.solarradiation, isStateChange: true)
+    sendEvent(name: "uv", value: weather.uv, isStateChange: true)
 
     //Indoor Temp
-    sendEvent(name: "indoortemperature", value: weather.tempinf, unit: '°F', isStateChange: true);
+    sendEvent(name: "indoortemperature", value: weather.tempinf, unit: '°F', isStateChange: true)
     
     //Indoor Humidity
     sendEvent(name: "indoorhumidity", value: weather.humidityin, unit: '%', isStateChange: true)
 
     if (weather.containsKey('temp1f')) {
         // We will assume all the item 1 keys exist
-        sendEvent(name: "temperature1", value: weather.temp1f, unit: '°F', isStateChange: true);
+        sendEvent(name: "temperature1", value: weather.temp1f, unit: '°F', isStateChange: true)
         sendEvent(name: "humidity1", value: weather.humidity1, unit: '%', isStateChange: true)
     }
 
     if (weather.containsKey('temp2f')) {
         // We will assume all the item 2 keys exist
-        sendEvent(name: "temperature2", value: weather.temp1f, unit: '°F', isStateChange: true);
+        sendEvent(name: "temperature2", value: weather.temp1f, unit: '°F', isStateChange: true)
         sendEvent(name: "humidity2", value: weather.humidity1, unit: '%', isStateChange: true)
     }
 
     if (weather.containsKey('temp3f')) {
         // We will assume all the item 3 keys exist
-        sendEvent(name: "temperature3", value: weather.temp1f, unit: '°F', isStateChange: true);
+        sendEvent(name: "temperature3", value: weather.temp1f, unit: '°F', isStateChange: true)
         sendEvent(name: "humidity3", value: weather.humidity1, unit: '%', isStateChange: true)
     }
 
     if (weather.containsKey('temp4f')) {
         // We will assume all the item 4 keys exist
-        sendEvent(name: "temperature4", value: weather.temp1f, unit: '°F', isStateChange: true);
+        sendEvent(name: "temperature4", value: weather.temp1f, unit: '°F', isStateChange: true)
         sendEvent(name: "humidity4", value: weather.humidity1, unit: '%', isStateChange: true)
     }
 
     if (weather.containsKey('temp5f')) {
         // We will assume all the item 5 keys exist
-        sendEvent(name: "temperature5", value: weather.temp1f, unit: '°F', isStateChange: true);
+        sendEvent(name: "temperature5", value: weather.temp1f, unit: '°F', isStateChange: true)
         sendEvent(name: "humidity5", value: weather.humidity1, unit: '%', isStateChange: true)
     }
 
     if (weather.containsKey('temp6f')) {
         // We will assume all the item 6 keys exist
-        sendEvent(name: "temperature6", value: weather.temp1f, unit: '°F', isStateChange: true);
+        sendEvent(name: "temperature6", value: weather.temp1f, unit: '°F', isStateChange: true)
         sendEvent(name: "humidity6", value: weather.humidity1, unit: '%', isStateChange: true)
     }
 
     if (weather.containsKey('temp7f')) {
         // We will assume all the item 7 keys exist
-        sendEvent(name: "temperature7", value: weather.temp1f, unit: '°F', isStateChange: true);
+        sendEvent(name: "temperature7", value: weather.temp1f, unit: '°F', isStateChange: true)
         sendEvent(name: "humidity7", value: weather.humidity1, unit: '%', isStateChange: true)
     }
 
     if (weather.containsKey('temp8f')) {
         // We will assume all the item 8 keys exist
-        sendEvent(name: "temperature8", value: weather.temp1f, unit: '°F', isStateChange: true);
+        sendEvent(name: "temperature8", value: weather.temp1f, unit: '°F', isStateChange: true)
         sendEvent(name: "humidity8", value: weather.humidity1, unit: '%', isStateChange: true)
     }
 }
